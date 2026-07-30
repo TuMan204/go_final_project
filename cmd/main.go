adffab0c0d4cc50bc7df406a29fed253ec90c50b
TuMan204 <maks.buzow@yandex.ru>
2026-07-31 01:43:52 +0300

    add server code and env*

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	_ "fmt"
+	"log"
+
+	"github.com/TuMan204/go_final_project/internal/server"
 )
 
 func main() {
+	var logger log.Logger
+	server := server.NewServer(&logger)
 
+	err := server.Serv.ListenAndServe()
+	if err != nil {
+		logger.Fatal()
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,39 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Server struct {
+	logger *log.Logger
+	Serv   *http.Server
+}
+
+func NewServer(logger *log.Logger) Server {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./web")))
+
+	addr := 7540
+	envPort := os.Getenv("TODO_PORT")
+	if len(envPort) > 0 {
+		port, err := strconv.Atoi(envPort)
+		if err == nil {
+			addr = port
+		}
+	}
+
+	serv := http.Server{
+		Addr:     fmt.Sprintf(":%d", addr),
+		Handler:  mux,
+		ErrorLog: logger,
+		//ReadTimeout:  10 * time.Second,
+		//WriteTimeout: 10 * time.Second,
+		//IdleTimeout:  15 * time.Second,
+	}
+
+	return Server{logger: logger, Serv: &serv}
+}
